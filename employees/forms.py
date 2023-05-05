9fd111f09839b4b553a2f4c46e3a3cc2d00bf573
maw619 <marcowolff619@gmail.com>
2023-05-05 03:16:08 -0400

    asdfcasdc

--- employees/forms.py
+++ employees/forms.py
@@ -8,11 +8,11 @@ from .models import SoEmployee, SoOut, SoType, Shift
 class SoOutForm(ModelForm):
     class Meta:
         model = SoOut
-        fields = ['co_fk_type_id_key','co_fk_em_id_key','co_date','co_time_dif','co_time_arrived']
-        labels = { 'co_fk_em_id_key': 'Employee' }
+        fields = ['co_date','co_time_dif','co_time_arrived','co_fk_type_id_key','co_fk_em_id_key']
+        labels = { 'co_fk_type_id_key':'Type','co_fk_em_id_key': 'Employee'}
         widgets = {
             'co_fk_em_id_key': forms.Select(attrs={'class':'form-control', 'id':'single1'}),
-            'co_fk_type_id_key': forms.Select(attrs={'class  mr-5':''}), 
+            'co_fk_type_id_key': forms.Select(attrs={'class':'form-control', 'id':'single3'}), 
             'co_time_arrived': forms.HiddenInput(),
             'co_date': forms.HiddenInput(),
             'co_time_dif': forms.HiddenInput()
